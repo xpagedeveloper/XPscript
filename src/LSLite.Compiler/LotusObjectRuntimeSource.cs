9151ac9a6ff9c4a9ec943c18e042d7e59ec69bcd
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-11 13:00:56 +0200

    Add LotusScript object reference runtime

---
+++ src/LSLite.Compiler/LotusObjectRuntimeSource.cs
@@ -0,0 +1,49 @@
+namespace LSLite.Compiler;
+
+public static class LotusObjectRuntimeSource
+{
+    public const string Code = """
+internal abstract class LSObjectBase
+{
+    private bool _deleted;
+
+    public bool __IsDeleted => _deleted;
+
+    public virtual void __Delete()
+    {
+        _deleted = true;
+    }
+}
+
+internal sealed class LSRef<T> where T : LSObjectBase
+{
+    public T? Value { get; private set; }
+
+    public bool IsNothing => Value is null;
+
+    public LSRef()
+    {
+    }
+
+    private LSRef(T value)
+    {
+        Value = value;
+    }
+
+    public static LSRef<T> Create(T value) => new(value);
+
+    public void Delete()
+    {
+        var value = Value;
+        if (value is null)
+            return;
+
+        value.__Delete();
+        Value = null;
+    }
+
+    public bool IsSameReference(LSRef<T>? other) =>
+        other is not null && ReferenceEquals(this, other);
+}
+""";
+}
