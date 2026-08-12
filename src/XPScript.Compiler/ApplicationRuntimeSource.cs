778d544ca578358c90162ab40a233895cb65034b
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-12 02:37:52 +0200

    Return defensive copies from Application.Args

--- src/XPScript.Compiler/ApplicationRuntimeSource.cs
+++ src/XPScript.Compiler/ApplicationRuntimeSource.cs
@@ -7,33 +7,23 @@ internal static class XPScriptApplicationRuntime
 {
     private static readonly object Sync = new();
     private static string[] _args = [];
-    private static LSArray? _argsArray;
 
     public static void SetArgs(string[]? args)
     {
         lock (Sync)
-        {
             _args = args is null ? [] : [.. args];
-            _argsArray = null;
-        }
     }
 
     public static LSArray Args()
     {
         lock (Sync)
         {
-            if (_argsArray is not null) return _argsArray;
-
             if (_args.Length == 0)
-            {
-                _argsArray = new LSArray("String", true);
-                return _argsArray;
-            }
+                return new LSArray("String", true);
 
             var array = new LSArray("String", true, [0], [_args.Length - 1]);
             for (var i = 0; i < _args.Length; i++)
                 array.Set(_args[i], i);
-            _argsArray = array;
             return array;
         }
     }
