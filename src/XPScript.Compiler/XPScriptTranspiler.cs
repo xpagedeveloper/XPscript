30932b811c6128bb3f54dd5aba02330bba54e9d6
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-15 13:52:18 +0200

    Remove experimental inline Error preprocessor

--- src/XPScript.Compiler/XPScriptTranspiler.cs
+++ src/XPScript.Compiler/XPScriptTranspiler.cs
@@ -60,7 +60,6 @@ public sealed class XPScriptTranspiler
         var operatorArray = new OperatorArrayCompatibilityPreprocessor();
         source = operatorArray.NormalizeSource(source);
         var protectedSource = ProtectStringLiterals(source, out var protectedStrings);
-        protectedSource = new InlineErrorStatementPreprocessor().Transform(protectedSource);
         protectedSource = new ApplicationObjectPreprocessor().Transform(protectedSource);
         protectedSource = RewriteListPresenceChecks(protectedSource);
         protectedSource = operatorArray.TransformProtectedSource(protectedSource);
