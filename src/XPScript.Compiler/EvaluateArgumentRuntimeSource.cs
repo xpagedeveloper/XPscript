f2c3e2139c2189194f606b0f7b4a76b69dd7888c
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-16 01:33:54 +0200

    Coerce Evaluate ByRef writeback to caller type

--- src/XPScript.Compiler/EvaluateArgumentRuntimeSource.cs
+++ src/XPScript.Compiler/EvaluateArgumentRuntimeSource.cs
@@ -17,8 +17,8 @@ internal sealed class XPScriptEvaluateArgument
     public bool IsByRef { get; }
     public object? Value { get; }
 
-    public static XPScriptEvaluateArgument ByRef(object? value, Action<object?> setter) =>
-        new(true, value, setter);
+    public static XPScriptEvaluateArgument ByRef<T>(T value, Action<T> setter) =>
+        new(true, value, incoming => setter(Coerce<T>(incoming)));
 
     public static XPScriptEvaluateArgument ByVal(object? value) =>
         new(false, value, null);
@@ -27,6 +27,25 @@ internal sealed class XPScriptEvaluateArgument
     {
         if (IsByRef)
             _setter!(value);
+    }
+
+    private static T Coerce<T>(object? value)
+    {
+        var target = typeof(T);
+        object? converted = target == typeof(string) ? XPScriptRuntime.CStr(value)
+            : target == typeof(byte) ? XPScriptRuntime.CByte(value)
+            : target == typeof(int) ? XPScriptRuntime.CInt(value)
+            : target == typeof(long) ? XPScriptRuntime.CLng(value)
+            : target == typeof(float) ? XPScriptRuntime.CSng(value)
+            : target == typeof(double) ? XPScriptRuntime.CDbl(value)
+            : target == typeof(decimal) ? XPScriptRuntime.CCur(value)
+            : target == typeof(bool) ? XPScriptRuntime.CBool(value)
+            : target == typeof(DateTime) ? XPScriptRuntime.CDat(value)
+            : value;
+
+        if (converted is T typed) return typed;
+        if (converted is null) return default!;
+        return (T)converted;
     }
 }
 """;
