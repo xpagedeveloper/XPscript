a8650b7b4956e5fad32f6cd3bdcd295347f9eeb9
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-16 11:06:55 +0200

    Write Shell marker to absolute GitHub workspace

--- samples/xps-shell-pathext.cmd
+++ samples/xps-shell-pathext.cmd
@@ -1,5 +1,5 @@
 @echo off
 if not "%~1"=="safe" exit /b 9
-if "%XPS_SHELL_MARKER%"=="" exit /b 10
-echo SHELL-WINDOWS-RESOLUTION=OK>"%XPS_SHELL_MARKER%"
+if "%GITHUB_WORKSPACE%"=="" exit /b 10
+echo SHELL-WINDOWS-RESOLUTION=OK>"%GITHUB_WORKSPACE%\shell-windows-resolution.txt"
 exit /b 0
