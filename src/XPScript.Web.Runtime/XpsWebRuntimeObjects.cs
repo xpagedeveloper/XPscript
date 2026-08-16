382707aa669ac8bf39e067a36e7c0e699e06e1f7
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-16 23:20:06 +0200

    Expose Session and Application runtime objects

--- src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
+++ src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
@@ -5,4 +5,8 @@ public static class XpsWebRuntimeObjects
     public static XpsWebRequest Request => XpsWebContextAccessor.Current.Request;
     public static XpsWebResponse Response => XpsWebContextAccessor.Current.Response;
     public static XpsWebServer Server => new(XpsWebContextAccessor.Current.Server);
+    public static IXpsApplicationState Application => XpsWebContextAccessor.Current.Application;
+    public static IXpsSession Session =>
+        XpsWebContextAccessor.Current.Session ??
+        throw new InvalidOperationException("Session support is not enabled for this XPScript site.");
 }
