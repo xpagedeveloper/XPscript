48cc416fd3ed79d10517a9ac2b62106f740b1e5b
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-16 23:34:43 +0200

    Remove temporary string compatibility helper

--- src/XPScript.Web.FastCgi/StringComparisonExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace XPScript.Web.FastCgi;
-
-internal static class StringComparisonExtensions
-{
-    internal static bool StartsWith(this string value, char prefix, StringComparison comparison) =>
-        value.StartsWith(prefix.ToString(), comparison);
-}
