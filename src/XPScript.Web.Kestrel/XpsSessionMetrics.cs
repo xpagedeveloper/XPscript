28ca2ac1c22002b4813e8c06825e24440a7e8f68
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-17 22:06:57 +0200

    Add active connection gauge to Prometheus output

--- src/XPScript.Web.Kestrel/XpsSessionMetrics.cs
+++ src/XPScript.Web.Kestrel/XpsSessionMetrics.cs
@@ -6,10 +6,17 @@ namespace XPScript.Web.Kestrel;
 
 internal static class XpsSessionMetrics
 {
-    public static string Render(XpsWebTelemetry telemetry, XpsSessionStore? sessions)
+    public static string Render(XpsWebTelemetry telemetry, XpsSessionStore? sessions, long activeConnections)
     {
         ArgumentNullException.ThrowIfNull(telemetry);
         var builder = new StringBuilder(telemetry.RenderPrometheus());
+
+        builder.Append("# TYPE xpscript_web_active_connections gauge\n");
+        builder.Append("# HELP xpscript_web_active_connections Current active Kestrel connections.\n");
+        builder.Append("xpscript_web_active_connections ")
+            .Append(Math.Max(0, activeConnections).ToString(CultureInfo.InvariantCulture))
+            .Append('\n');
+
         if (sessions is null) return builder.ToString();
 
         builder.Append("# TYPE xpscript_web_sessions_active gauge\n");
