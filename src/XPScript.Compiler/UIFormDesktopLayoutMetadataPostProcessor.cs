6134c67119f391da95ddc370b67db5d15de2baba
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-18 23:21:19 +0200

    Pass UIForm actions through desktop bridge

--- src/XPScript.Compiler/UIFormDesktopLayoutMetadataPostProcessor.cs
+++ src/XPScript.Compiler/UIFormDesktopLayoutMetadataPostProcessor.cs
@@ -25,7 +25,37 @@ internal sealed class UIFormDesktopLayoutMetadataPostProcessor
                 rowSpan = field.RowSpan,
                 regionId = field.RegionId,
                 refreshTargetRegion = field.RefreshTargetRegion,
-                refreshHandler = field.RefreshHandler
+                refreshHandler = field.RefreshHandler,
+                onChangeHandler = field.OnChangeHandler,
+                visible = field.Visible,
+                enabled = field.Enabled,
+                readOnly = field.ReadOnly
+""");
+
+        generated = ReplaceRequired(generated,
+            "                options = field.Options,\n                layoutRow = field.LayoutRow,",
+            """
+                options = field.Options,
+                layoutRow = field.LayoutRow,
+""");
+
+        generated = ReplaceRequired(generated,
+            "            }).ToArray()\n        };\n",
+            """
+            }).ToArray(),
+            buttons = form.Buttons.Select(button => new
+            {
+                name = button.Name,
+                label = button.Label,
+                style = button.Style,
+                layoutRow = button.LayoutRow,
+                layoutColumn = button.LayoutColumn,
+                columnSpan = button.ColumnSpan,
+                rowSpan = button.RowSpan,
+                visible = button.Visible,
+                enabled = button.Enabled
+            }).ToArray()
+        };
 """);
 
         return generated;
