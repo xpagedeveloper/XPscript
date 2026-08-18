901b798356c22748ecfa074b4377dd53460828ce
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-18 23:54:34 +0200

    Wire UIListView event postprocessor

--- src/XPScript.Compiler/UIExtensionDesktopPostProcessor.cs
+++ src/XPScript.Compiler/UIExtensionDesktopPostProcessor.cs
@@ -58,6 +58,7 @@ internal sealed class UIExtensionDesktopPostProcessor
         replaced = new UIFormDesktopLayoutMetadataPostProcessor().Transform(replaced);
         replaced = new UIFormDesktopReactivePostProcessor().Transform(replaced);
         replaced = new UIListViewDesktopPostProcessor().Transform(replaced);
+        replaced = new UIListViewEventPostProcessor().Transform(replaced);
         return new UIFormWebPartialRefreshPostProcessor().Transform(replaced);
     }
 
