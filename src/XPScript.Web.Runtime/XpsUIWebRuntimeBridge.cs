315357fbbf0e80466668d9664c9a2a82659f5f7b
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-19 14:52:07 +0200

    Load Bootstrap 5.3.8 once for web UI rendering

--- src/XPScript.Web.Runtime/XpsUIWebRuntimeBridge.cs
+++ src/XPScript.Web.Runtime/XpsUIWebRuntimeBridge.cs
@@ -2,6 +2,14 @@ namespace XPScript.Web.Runtime;
 
 public static class XpsUIWebRuntimeBridge
 {
+    private const string BootstrapCss = "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB\" crossorigin=\"anonymous\">";
+    private static readonly System.Runtime.CompilerServices.ConditionalWeakTable<XpsWebResponse, BootstrapState> BootstrapStates = new();
+
+    private sealed class BootstrapState
+    {
+        public bool Written;
+    }
+
     public static bool IsAvailable()
     {
         try
@@ -28,6 +36,18 @@ public static class XpsUIWebRuntimeBridge
         ArgumentNullException.ThrowIfNull(html);
         var response = XpsWebContextAccessor.Current.Response;
         response.ContentType = "text/html; charset=utf-8";
+        EnsureBootstrap(response);
         response.Write(html);
+    }
+
+    private static void EnsureBootstrap(XpsWebResponse response)
+    {
+        var state = BootstrapStates.GetOrCreateValue(response);
+        lock (state)
+        {
+            if (state.Written) return;
+            response.Write(BootstrapCss);
+            state.Written = true;
+        }
     }
 }
