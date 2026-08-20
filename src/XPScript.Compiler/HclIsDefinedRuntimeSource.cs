2dcb039862e646b6310221c8efe2999b50d5dac6
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-20 10:52:38 +0200

    Implement HCL IsDefined platform constants

---
+++ src/XPScript.Compiler/HclIsDefinedRuntimeSource.cs
@@ -0,0 +1,30 @@
+namespace XPScript.Compiler;
+
+internal static class HclIsDefinedRuntimeSource
+{
+    public const string Code = """
+internal static class LSHclPlatformConstantRuntime
+{
+    public static bool IsDefined(object? value)
+    {
+        if (XPScriptNullRuntime.IsNull(value)) return false;
+        var name = XPScriptRuntime.CStr(value).Trim().ToUpperInvariant();
+        if (name.Length == 0) return false;
+
+        return name switch
+        {
+            "WINDOWS" => OperatingSystem.IsWindows(),
+            "WIN32" => OperatingSystem.IsWindows(),
+            "WIN16" => false,
+            "UNIX" => OperatingSystem.IsLinux() || OperatingSystem.IsMacOS() || OperatingSystem.IsFreeBSD(),
+            "LINUX" => OperatingSystem.IsLinux(),
+            "MAC" => false,
+            "MAC68K" => false,
+            "MACPPC" => false,
+            "OLE" => OperatingSystem.IsWindows(),
+            _ => false
+        };
+    }
+}
+""";
+}
