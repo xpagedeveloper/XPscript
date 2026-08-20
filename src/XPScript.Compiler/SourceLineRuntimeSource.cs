0ef9b5a7c2428a89835dd316a02773fa712a5442
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-20 21:21:04 +0200

    Track current XPScript source file at runtime

--- src/XPScript.Compiler/SourceLineRuntimeSource.cs
+++ src/XPScript.Compiler/SourceLineRuntimeSource.cs
@@ -6,17 +6,26 @@ internal static class SourceLineRuntimeSource
 internal static class XPSourceLineRuntime
 {
     [ThreadStatic] private static int _current;
+    [ThreadStatic] private static string? _currentFile;
 
     public static int Current => _current;
+    public static string CurrentFile => _currentFile ?? "";
 
     public static void Set(int line)
     {
         _current = line < 0 ? 0 : line;
     }
 
+    public static void Set(int line, string? file)
+    {
+        _current = line < 0 ? 0 : line;
+        _currentFile = file ?? "";
+    }
+
     public static void Clear()
     {
         _current = 0;
+        _currentFile = "";
     }
 }
 """;
