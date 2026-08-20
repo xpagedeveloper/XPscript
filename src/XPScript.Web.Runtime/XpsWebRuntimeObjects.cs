f45cc3ac4650004ff65710b0cbf668781bdc0d3e
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-20 09:06:44 +0200

    Expose Body as reserved web runtime object

--- src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
+++ src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
@@ -4,6 +4,7 @@ public static class XpsWebRuntimeObjects
 {
     public static XpsWebRequest Request => XpsWebContextAccessor.Current.Request;
     public static XpsWebResponse Response => XpsWebContextAccessor.Current.Response;
+    public static XpsRequestBody Body => new(XpsWebContextAccessor.Current.Request);
     public static XpsWebServer Server => new(XpsWebContextAccessor.Current.Server);
     public static IXpsRequestState RequestScope => XpsWebContextAccessor.Current.RequestScope;
     public static IXpsApplicationState Application => XpsWebContextAccessor.Current.Application;
