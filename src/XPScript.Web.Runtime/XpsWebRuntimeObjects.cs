567f68e871126d51bd61dc1ee96def0fbcb8209a
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-21 08:09:15 +0200

    Expose process and application state objects

--- src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
+++ src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
@@ -2,12 +2,16 @@ namespace XPScript.Web.Runtime;
 
 public static class XpsWebRuntimeObjects
 {
+    private static readonly XpsProcessState SharedProcessState = new();
+    private static readonly XpsWebProcess SharedProcess = new(SharedProcessState);
+
     public static XpsWebRequest Request => XpsWebContextAccessor.Current.Request;
     public static XpsWebResponse Response => XpsWebContextAccessor.Current.Response;
     public static XpsRequestBody Body => new(XpsWebContextAccessor.Current.Request);
     public static XpsWebServer Server => new(XpsWebContextAccessor.Current.Server);
     public static IXpsRequestState RequestScope => XpsWebContextAccessor.Current.RequestScope;
-    public static IXpsApplicationState Application => XpsWebContextAccessor.Current.Application;
+    public static XpsWebProcess Process => SharedProcess;
+    public static XpsWebApplication Application => new(XpsWebContextAccessor.Current.Application);
     public static IXpsSession Session =>
         XpsWebContextAccessor.Current.Session ??
         throw new InvalidOperationException("Session support is not enabled for this XPScript site.");
