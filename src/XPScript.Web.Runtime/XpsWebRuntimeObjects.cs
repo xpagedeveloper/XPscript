e902988cbc576800cd7a18e63fe157fd7cd1ed8f
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-21 16:47:49 +0200

    Consume one-hop Request.State lazily

--- src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
+++ src/XPScript.Web.Runtime/XpsWebRuntimeObjects.cs
@@ -9,7 +9,20 @@ public static class XpsWebRuntimeObjects
     public static XpsWebResponse Response => XpsWebContextAccessor.Current.Response;
     public static XpsRequestBody Body => new(XpsWebContextAccessor.Current.Request);
     public static XpsWebServer Server => new(XpsWebContextAccessor.Current.Server);
-    public static IXpsRequestState RequestScope => XpsWebContextAccessor.Current.RequestScope;
+    public static IXpsRequestState RequestScope
+    {
+        get
+        {
+            var context = XpsWebContextAccessor.Current;
+            var inherited = XpsNavigationStateHandoff.TryConsume(context.Request, context.Response);
+            if (inherited is not null)
+            {
+                foreach (var key in inherited.Keys)
+                    context.RequestScope.Set(key, inherited.Get(key));
+            }
+            return context.RequestScope;
+        }
+    }
     public static XpsWebProcess Process => SharedProcess;
     public static IXpsApplicationState Application => XpsWebContextAccessor.Current.Application;
     public static IXpsSession Session =>
@@ -18,4 +31,17 @@ public static class XpsWebRuntimeObjects
 
     public static void StageRequestStateForNavigation()
         => XpsNavigationStateHandoff.StageCurrent();
+
+    public static bool TryStageRequestStateForNavigation()
+    {
+        try
+        {
+            XpsNavigationStateHandoff.StageCurrent();
+            return true;
+        }
+        catch (InvalidOperationException ex) when (ex.Message.Contains("No XPScript web request context is active", StringComparison.Ordinal))
+        {
+            return false;
+        }
+    }
 }
