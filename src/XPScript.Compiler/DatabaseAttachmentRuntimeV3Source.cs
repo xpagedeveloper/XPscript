7874207adfea42b91798357a9fac809b4e9648c9
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-23 16:01:14 +0200

    Align attachment API wrapper with metadata contract

--- src/XPScript.Compiler/DatabaseAttachmentRuntimeV3Source.cs
+++ src/XPScript.Compiler/DatabaseAttachmentRuntimeV3Source.cs
@@ -12,65 +12,33 @@ internal sealed class XPScriptAttachmentCollectionV3
         _inner = inner;
     }
 
-    public XPScriptJsonArray List() => CleanArray(_inner.List());
-    public XPScriptJsonArray GetMetadata() => CleanArray(_inner.GetMetadata());
-    public XPScriptJsonObject GetMetadata(object? attachmentId) => CleanObject(_inner.GetMetadata(attachmentId));
-    public XPScriptJsonArray FindByName(object? originalName) => CleanArray(_inner.FindByName(originalName));
+    public void SetActor(object? actor) => _inner.SetActor(actor);
 
-    public XPScriptJsonObject Save(object? sourcePath, object? createdBy)
-    {
-        var actor = RequiredActor(createdBy);
-        _inner.SetActor(actor);
-        return CleanObject(_inner.Save(sourcePath));
-    }
+    public XPScriptJsonArray List() => _inner.List();
+    public XPScriptJsonArray GetMetadata() => _inner.GetMetadata();
+    public XPScriptJsonObject GetMetadata(object? attachmentId) => _inner.GetMetadata(attachmentId);
+    public XPScriptJsonArray FindByName(object? originalName) => _inner.FindByName(originalName);
 
-    public XPScriptJsonObject SaveAs(object? sourcePath, object? originalName, object? createdBy)
-    {
-        var actor = RequiredActor(createdBy);
-        _inner.SetActor(actor);
-        return CleanObject(_inner.SaveAs(sourcePath, originalName));
-    }
+    public XPScriptJsonObject Save(object? sourcePath)
+        => _inner.Save(sourcePath);
+
+    public XPScriptJsonObject SaveAs(object? sourcePath, object? originalName)
+        => _inner.SaveAs(sourcePath, originalName);
+
+    public XPScriptJsonObject Update(object? attachmentId, object? sourcePath)
+        => _inner.Update(attachmentId, sourcePath);
+
+    public XPScriptJsonObject UpdateAs(object? attachmentId, object? sourcePath, object? originalName)
+        => _inner.UpdateAs(attachmentId, sourcePath, originalName);
 
     public bool Get(object? attachmentId, object? targetPath)
         => _inner.Get(attachmentId, targetPath);
 
     public XPScriptJsonArray GetAll(object? targetFolder)
-        => CleanArray(_inner.GetAll(targetFolder));
+        => _inner.GetAll(targetFolder);
 
     public bool Delete(object? attachmentId)
         => _inner.Delete(attachmentId);
-
-    private static string RequiredActor(object? value)
-    {
-        var actor = XPScriptRuntime.CStr(value).Trim();
-        if (actor.Length is < 1 or > 512 || actor.IndexOfAny(['\0', '\r', '\n']) >= 0)
-            throw new XPScriptRuntimeException(5, "Attachment createdBy must contain 1 to 512 characters without control line breaks.");
-        return actor;
-    }
-
-    private static XPScriptJsonArray CleanArray(XPScriptJsonArray source)
-    {
-        var result = new System.Text.Json.Nodes.JsonArray();
-        foreach (var node in source.Node)
-        {
-            if (node is System.Text.Json.Nodes.JsonObject obj)
-                result.Add(CleanNode(obj));
-            else
-                result.Add(node?.DeepClone());
-        }
-        return new XPScriptJsonArray(result);
-    }
-
-    private static XPScriptJsonObject CleanObject(XPScriptJsonObject source)
-        => new(CleanNode(source.Node));
-
-    private static System.Text.Json.Nodes.JsonObject CleanNode(System.Text.Json.Nodes.JsonObject source)
-    {
-        var obj = (System.Text.Json.Nodes.JsonObject)source.DeepClone();
-        obj.Remove("modified");
-        obj.Remove("modifiedBy");
-        return obj;
-    }
 }
 
 internal static class XPScriptDatabaseAttachmentApi
