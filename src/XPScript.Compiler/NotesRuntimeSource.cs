1c30f4d811c4e0633e3abf346c5737f7977698ed
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-25 21:40:53 +0200

    Consolidate Notes item runtime sources

--- src/XPScript.Compiler/NotesRuntimeSource.cs
+++ src/XPScript.Compiler/NotesRuntimeSource.cs
@@ -7,6 +7,5 @@ internal static class NotesRuntimeSource
         NotesRuntimeValueSource.Code + "\n\n" +
         NotesRuntimeDataSource.Code + "\n\n" +
         NotesRuntimeItemSource.Code + "\n\n" +
-        NotesRuntimeAttachmentSource.Code + "\n\n" +
         NotesNativeApiSource.Code;
 }
