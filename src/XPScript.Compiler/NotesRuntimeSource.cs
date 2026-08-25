d84b1aea744246a7b0b3b6591d8a7074738445cc
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-25 20:33:15 +0200

    Add modular Notes runtime aggregator

---
+++ src/XPScript.Compiler/NotesRuntimeSource.cs
@@ -0,0 +1,10 @@
+namespace XPScript.Compiler;
+
+internal static class NotesRuntimeSource
+{
+    public static string Code =>
+        NotesRuntimeCoreSource.Code + "\n\n" +
+        NotesRuntimeValueSource.Code + "\n\n" +
+        NotesRuntimeDataSource.Code + "\n\n" +
+        NotesNativeApiSource.Code;
+}
