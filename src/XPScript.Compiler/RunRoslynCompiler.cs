13212ca363af4f4b3280e0c7287f4a82fe42f522
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-26 14:18:46 +0200

    Fix Roslyn line position type import

--- src/XPScript.Compiler/RunRoslynCompiler.cs
+++ src/XPScript.Compiler/RunRoslynCompiler.cs
@@ -1,6 +1,7 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Emit;
+using Microsoft.CodeAnalysis.Text;
 using System.Collections.Immutable;
 using System.Text;
 using System.Text.Json;
