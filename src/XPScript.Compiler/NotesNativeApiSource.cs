91e208d10302ac02ddbee3ad4246f9746f71c395
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-26 12:00:20 +0200

    Include Notes ID password runtime source

--- src/XPScript.Compiler/NotesNativeApiSource.cs
+++ src/XPScript.Compiler/NotesNativeApiSource.cs
@@ -4,6 +4,7 @@ internal static class NotesNativeApiSource
 {
     public static string Code =>
         NotesNativeApiBaseSource.Code + "\n\n" +
+        NotesNativeApiPasswordSource.Code + "\n\n" +
         NotesNativeApiVersionSource.Code + "\n\n" +
         NotesNativeApiTimeSource.Code + "\n\n" +
         NotesNativeApiDatabaseSource.Code + "\n\n" +
