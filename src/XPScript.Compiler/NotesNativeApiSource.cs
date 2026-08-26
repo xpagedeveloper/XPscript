f4730fb5151b9b386e2bbe0d3f71ee42da42140a
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-26 08:02:26 +0200

    Wire NotesDatabase metadata native source

--- src/XPScript.Compiler/NotesNativeApiSource.cs
+++ src/XPScript.Compiler/NotesNativeApiSource.cs
@@ -6,6 +6,7 @@ internal static class NotesNativeApiSource
         NotesNativeApiBaseSource.Code + "\n\n" +
         NotesNativeApiVersionSource.Code + "\n\n" +
         NotesNativeApiTimeSource.Code + "\n\n" +
+        NotesNativeApiDatabaseSource.Code + "\n\n" +
         NotesNativeApiDocumentSource.Code + "\n\n" +
         NotesNativeApiItemSource.Code + "\n\n" +
         NotesNativeApiAttachmentSource.Code + "\n\n" +
