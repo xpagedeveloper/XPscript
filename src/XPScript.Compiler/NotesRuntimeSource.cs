214e817015b6e6c23ef65a591807c09c39c4b22c
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-29 20:21:34 +0200

    Wire NotesView navigation V1 into runtime pipeline

--- src/XPScript.Compiler/NotesRuntimeSource.cs
+++ src/XPScript.Compiler/NotesRuntimeSource.cs
@@ -7,14 +7,15 @@ internal static class NotesRuntimeSource
             NotesDocumentLotusScriptSurfacePostProcessor.Apply(
                 NotesDocumentRemovePostProcessor.Apply(
                     NotesDatabaseCreateCompatibilityPostProcessor.Apply(
-                        NotesViewColumnNamesPostProcessor.Apply(
-                            NotesDatabaseLifecyclePostProcessor.Apply(
-                                NotesDatabaseLotusScriptSurfacePostProcessor.Apply(
-                                    NotesDocumentCollectionPostProcessor.Apply(
-                                        NotesRuntimeCoreSource.Code + "\n\n" +
-                                        NotesRuntimeValueSource.Code + "\n\n" +
-                                        NotesRuntimeDataSource.Code + "\n\n" +
-                                        NotesRuntimeItemSource.Code + "\n\n" +
-                                        NotesRuntimeIndexedValueSource.Code + "\n\n" +
-                                        NotesNativeApiSource.Code)))))))));
+                        NotesViewNavigationPostProcessor.Apply(
+                            NotesViewColumnNamesPostProcessor.Apply(
+                                NotesDatabaseLifecyclePostProcessor.Apply(
+                                    NotesDatabaseLotusScriptSurfacePostProcessor.Apply(
+                                        NotesDocumentCollectionPostProcessor.Apply(
+                                            NotesRuntimeCoreSource.Code + "\n\n" +
+                                            NotesRuntimeValueSource.Code + "\n\n" +
+                                            NotesRuntimeDataSource.Code + "\n\n" +
+                                            NotesRuntimeItemSource.Code + "\n\n" +
+                                            NotesRuntimeIndexedValueSource.Code + "\n\n" +
+                                            NotesNativeApiSource.Code))))))))));
 }
