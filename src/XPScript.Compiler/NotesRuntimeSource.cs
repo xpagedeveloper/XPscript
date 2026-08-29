4f786c9ef1d61734b45eaf38fd6dcab64d14a45e
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-29 10:30:10 +0200

    Wire NotesDatabase lifecycle surface into runtime

--- src/XPScript.Compiler/NotesRuntimeSource.cs
+++ src/XPScript.Compiler/NotesRuntimeSource.cs
@@ -6,12 +6,13 @@ internal static class NotesRuntimeSource
         NotesDocumentComputeWithFormPostProcessor.Apply(
             NotesDocumentLotusScriptSurfacePostProcessor.Apply(
                 NotesDocumentRemovePostProcessor.Apply(
-                    NotesDatabaseLotusScriptSurfacePostProcessor.Apply(
-                        NotesDocumentCollectionPostProcessor.Apply(
-                            NotesRuntimeCoreSource.Code + "\n\n" +
-                            NotesRuntimeValueSource.Code + "\n\n" +
-                            NotesRuntimeDataSource.Code + "\n\n" +
-                            NotesRuntimeItemSource.Code + "\n\n" +
-                            NotesRuntimeIndexedValueSource.Code + "\n\n" +
-                            NotesNativeApiSource.Code))))));
+                    NotesDatabaseLifecyclePostProcessor.Apply(
+                        NotesDatabaseLotusScriptSurfacePostProcessor.Apply(
+                            NotesDocumentCollectionPostProcessor.Apply(
+                                NotesRuntimeCoreSource.Code + "\n\n" +
+                                NotesRuntimeValueSource.Code + "\n\n" +
+                                NotesRuntimeDataSource.Code + "\n\n" +
+                                NotesRuntimeItemSource.Code + "\n\n" +
+                                NotesRuntimeIndexedValueSource.Code + "\n\n" +
+                                NotesNativeApiSource.Code)))))));
 }
