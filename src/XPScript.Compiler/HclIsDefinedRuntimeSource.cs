0be8a08c0186861feda2067b7bcd36b4bce1abf5
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 20:16:32 +0200

    Wire DXL exporter result log into Notes runtime

--- src/XPScript.Compiler/HclIsDefinedRuntimeSource.cs
+++ src/XPScript.Compiler/HclIsDefinedRuntimeSource.cs
@@ -31,12 +31,13 @@ internal static class LSHclPlatformConstantRuntime
             NotesGetViewNotFoundPostProcessor.ApplyBuiltSurface(
                 NotesSessionPathPostProcessor.ApplyBuiltSurface(
                     NotesRuntimeMemberTrapPostProcessor.ApplyBuiltSurface(
-                        NotesDxlImportResultPostProcessor.ApplyBuiltSurface(
-                            NotesDatabaseReplicaPostProcessor.Apply(
-                                NotesDatabaseCreateCompatibilityPostProcessor.ApplyBuiltSurface(
-                                    NotesViewNavigatorCachePostProcessor.ApplyBuiltSurface(
-                                        NotesViewNavigationPostProcessor.ApplyBuiltSurface(
-                                            NotesDatabaseLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
-                                                NotesDocumentLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
-                                                    NotesRuntimeSourceBuilder.Build()))))))))))));
+                        NotesDxlExportResultPostProcessor.ApplyBuiltSurface(
+                            NotesDxlImportResultPostProcessor.ApplyBuiltSurface(
+                                NotesDatabaseReplicaPostProcessor.Apply(
+                                    NotesDatabaseCreateCompatibilityPostProcessor.ApplyBuiltSurface(
+                                        NotesViewNavigatorCachePostProcessor.ApplyBuiltSurface(
+                                            NotesViewNavigationPostProcessor.ApplyBuiltSurface(
+                                                NotesDatabaseLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
+                                                    NotesDocumentLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
+                                                        NotesRuntimeSourceBuilder.Build())))))))))))));
 }
