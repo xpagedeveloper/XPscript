6ea8968529ae1350c33deb0f2d2904b8bf62ef7b
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 21:59:58 +0200

    Use one unified version banner for version commands

--- src/XPScript.Cli/XPScriptCliStartupBanner.cs
+++ src/XPScript.Cli/XPScriptCliStartupBanner.cs
@@ -9,17 +9,21 @@ internal static class XPScriptCliStartupBanner
     internal static void Write()
     {
         var arguments = Environment.GetCommandLineArgs();
+        var commandArguments = arguments.Skip(1).ToArray();
         var assembly = Assembly.GetExecutingAssembly();
         var buildDate = assembly
             .GetCustomAttributes<AssemblyMetadataAttribute>()
             .FirstOrDefault(attribute => string.Equals(attribute.Key, "XPScriptBuildDate", StringComparison.Ordinal))
             ?.Value ?? "unknown";
 
-        if (arguments.Any(argument => argument.Equals("--debug", StringComparison.OrdinalIgnoreCase)))
+        if (commandArguments.Any(argument => argument.Equals("--debug", StringComparison.OrdinalIgnoreCase)))
             Environment.SetEnvironmentVariable("XPSCRIPT_RUNTIME_DEBUG", "1");
 
-        if (ShouldWriteBanner(arguments))
+        if (ShouldWriteBanner(commandArguments))
             Console.WriteLine($"XPScript version 0.9 Beta - build {buildDate} - XPageDeveloper.com ©");
+
+        if (commandArguments.Length == 1 && commandArguments[0] is "--version" or "--info" or "--debug")
+            Environment.Exit(0);
     }
 
     private static bool ShouldWriteBanner(string[] arguments)
