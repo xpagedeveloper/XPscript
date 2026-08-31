4e6e15351b9f4c42db38b71304f125b91eb728a1
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 16:36:00 +0200

    Print XPscript banner and make debug include info

--- src/XPScript.Compiler/Program.cs
+++ src/XPScript.Compiler/Program.cs
@@ -1,3 +1,28 @@
+using System.Reflection;
 using XPScript.Compiler;
 
-return await XPScriptCompilerCommandLine.RunAsync(args);
+var buildDate = Assembly.GetExecutingAssembly()
+    .GetCustomAttributes<AssemblyMetadataAttribute>()
+    .FirstOrDefault(attribute => string.Equals(attribute.Key, "XPScriptBuildDate", StringComparison.Ordinal))
+    ?.Value ?? "unknown";
+
+Console.WriteLine($"XPScript version 0.9 Beta - build {buildDate}");
+Console.WriteLine("XPageDeveloper.com (c)");
+Console.WriteLine();
+
+return await XPScriptCompilerCommandLine.RunAsync(NormalizeArguments(args));
+
+static string[] NormalizeArguments(string[] arguments)
+{
+    if (arguments.Length == 0 ||
+        !arguments[0].Equals("run", StringComparison.OrdinalIgnoreCase) ||
+        !arguments.Contains("--debug", StringComparer.OrdinalIgnoreCase) ||
+        arguments.Contains("--info", StringComparer.OrdinalIgnoreCase))
+        return arguments;
+
+    var result = arguments.ToList();
+    var separator = result.FindIndex(value => value == "--");
+    if (separator < 0) result.Add("--info");
+    else result.Insert(separator, "--info");
+    return result.ToArray();
+}
