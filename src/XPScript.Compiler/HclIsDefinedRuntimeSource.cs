d041a869813ab41b97dad54377b06594a7c25898
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 20:34:59 +0200

    Wire NotesNoteCollection into generated runtime

--- src/XPScript.Compiler/HclIsDefinedRuntimeSource.cs
+++ src/XPScript.Compiler/HclIsDefinedRuntimeSource.cs
@@ -31,13 +31,14 @@ internal static class LSHclPlatformConstantRuntime
             NotesGetViewNotFoundPostProcessor.ApplyBuiltSurface(
                 NotesSessionPathPostProcessor.ApplyBuiltSurface(
                     NotesRuntimeMemberTrapPostProcessor.ApplyBuiltSurface(
-                        NotesDxlExportResultPostProcessor.ApplyBuiltSurface(
-                            NotesDxlImportResultPostProcessor.ApplyBuiltSurface(
-                                NotesDatabaseReplicaPostProcessor.Apply(
-                                    NotesDatabaseCreateCompatibilityPostProcessor.ApplyBuiltSurface(
-                                        NotesViewNavigatorCachePostProcessor.ApplyBuiltSurface(
-                                            NotesViewNavigationPostProcessor.ApplyBuiltSurface(
-                                                NotesDatabaseLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
-                                                    NotesDocumentLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
-                                                        NotesRuntimeSourceBuilder.Build())))))))))))));
+                        NotesNoteCollectionPostProcessor.ApplyBuiltSurface(
+                            NotesDxlExportResultPostProcessor.ApplyBuiltSurface(
+                                NotesDxlImportResultPostProcessor.ApplyBuiltSurface(
+                                    NotesDatabaseReplicaPostProcessor.Apply(
+                                        NotesDatabaseCreateCompatibilityPostProcessor.ApplyBuiltSurface(
+                                            NotesViewNavigatorCachePostProcessor.ApplyBuiltSurface(
+                                                NotesViewNavigationPostProcessor.ApplyBuiltSurface(
+                                                    NotesDatabaseLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
+                                                        NotesDocumentLotusScriptSurfacePostProcessor.ApplyBuiltSurface(
+                                                            NotesRuntimeSourceBuilder.Build()))))))))))))));
 }
