18f8e3202a3b4e8b4f15ed3e03797063f0d3cb9b
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 15:40:51 +0200

    Route parameterless NotesSession through autodetect preprocessor

--- src/XPScript.Compiler/UIExtensionPreprocessor.cs
+++ src/XPScript.Compiler/UIExtensionPreprocessor.cs
@@ -54,7 +54,8 @@ internal sealed class UIExtensionPreprocessor
             output.Add(indent + rewritten);
         }
 
-        var transformed = new NotesRuntimePreprocessor().Transform(string.Join(Environment.NewLine, output));
+        var notesPrepared = new NotesSessionAutoDetectPreprocessor().Transform(string.Join(Environment.NewLine, output));
+        var transformed = new NotesRuntimePreprocessor().Transform(notesPrepared);
         return new AttachmentCollectionPreprocessor().Transform(transformed);
     }
 
