9d8d064de50d5c7e1fabad8dd4031b61c4e063b2
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 22:00:12 +0200

    Remove legacy duplicate CLI version initializer

--- src/XPScript.Cli/XPScriptVersion.cs
+++ src/XPScript.Cli/XPScriptVersion.cs
@@ -1,28 +1,7 @@
-using System.Runtime.CompilerServices;
-
 namespace XPScript.Cli;
 
 internal static class XPScriptVersion
 {
-    public const string Version = "1.0";
-    public const string Copyright = "(c) XPageDeveloper.com 2026";
-    public static string Banner => $"XPScript Version {Version} - {Copyright}";
-
-    [ModuleInitializer]
-    internal static void InitializeCommandLineVersionOutput()
-    {
-        var args = Environment.GetCommandLineArgs().Skip(1).ToArray();
-
-        if (args.Length == 0)
-        {
-            Console.WriteLine(Banner);
-            return;
-        }
-
-        if (args.Length == 1 && args[0] is "--version" or "--info" or "--debug")
-        {
-            Console.WriteLine(Banner);
-            Environment.Exit(0);
-        }
-    }
+    public const string Version = "0.9 Beta";
+    public const string Copyright = "XPageDeveloper.com ©";
 }
