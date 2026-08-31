eeea548615a269a4205bf1c25a84b8cee8ab429a
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 17:10:27 +0200

    Read build date from unified xpscript assembly

--- src/XPScript.Cli/XPScriptCliStartupBanner.cs
+++ src/XPScript.Cli/XPScriptCliStartupBanner.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using XPScript.Compiler;
 
 namespace XPScript.Cli;
 
@@ -9,8 +8,8 @@ internal static class XPScriptCliStartupBanner
     [ModuleInitializer]
     internal static void Write()
     {
-        var compilerAssembly = typeof(XPScriptCompilerCommandLine).Assembly;
-        var buildDate = compilerAssembly
+        var assembly = Assembly.GetExecutingAssembly();
+        var buildDate = assembly
             .GetCustomAttributes<AssemblyMetadataAttribute>()
             .FirstOrDefault(attribute => string.Equals(attribute.Key, "XPScriptBuildDate", StringComparison.Ordinal))
             ?.Value ?? "unknown";
