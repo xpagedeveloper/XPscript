d957a9a4b33e96833da6ab6d95314557d571fff9
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-08-31 16:25:41 +0200

    Print XPscript startup version banner

--- src/XPScript.Compiler/Program.cs
+++ src/XPScript.Compiler/Program.cs
@@ -1,3 +1,14 @@
+using System.Reflection;
 using XPScript.Compiler;
+
+var assembly = Assembly.GetExecutingAssembly();
+var buildDate = assembly
+    .GetCustomAttributes<AssemblyMetadataAttribute>()
+    .FirstOrDefault(attribute => attribute.Key == "XPScriptBuildDate")?.Value
+    ?? "unknown";
+
+Console.WriteLine($"XPScript version 0.9 Beta - build {buildDate}");
+Console.WriteLine("XPageDeveloper.com (c)");
+Console.WriteLine();
 
 return await XPScriptCompilerCommandLine.RunAsync(args);
