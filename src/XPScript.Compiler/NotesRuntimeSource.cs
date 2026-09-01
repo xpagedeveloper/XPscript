da95d5ea2632d64d6b028d107dcc90054926d2f1
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-01 12:11:35 +0200

    Wire rich text attachment insertion runtime

--- src/XPScript.Compiler/NotesRuntimeSource.cs
+++ src/XPScript.Compiler/NotesRuntimeSource.cs
@@ -8,35 +8,36 @@ internal static class NotesRuntimeSource
         {
             var source = NotesEmbeddedBinaryArrayFixPostProcessor.Apply(
                 NotesRichTextNavigatorElementPostProcessor.Apply(
-                    NotesEmbeddedObjectPostProcessor.Apply(
-                        NotesRichTextRangePostProcessor.Apply(
-                            NotesRichTextObjectsPostProcessor.Apply(
-                                NotesRichTextMimePostProcessor.Apply(
-                                    NotesThreadLifecyclePostProcessor.Apply(
-                                        NotesDxlPostProcessor.Apply(
-                                            NotesDocumentComputeWithFormPostProcessor.Apply(
-                                                NotesDocumentLotusScriptSurfacePostProcessor.Apply(
-                                                    NotesDocumentRemovePostProcessor.Apply(
-                                                        NotesDatabaseCreateCompatibilityPostProcessor.Apply(
-                                                            NotesViewNavigatorHistoryCapPostProcessor.Apply(
-                                                                NotesViewNavigatorBufferMaxEntriesPostProcessor.Apply(
-                                                                    NotesViewNavigatorCachePostProcessor.Apply(
-                                                                        NotesViewNavigatorCachePolicyPostProcessor.Apply(
-                                                                            NotesViewNavigationV3FixPostProcessor.Apply(
-                                                                                NotesViewNavigationV3PostProcessor.Apply(
-                                                                                    NotesViewNavigationV2FixPostProcessor.Apply(
-                                                                                        NotesViewNavigationV2PostProcessor.Apply(
-                                                                                            NotesViewNavigationPostProcessor.Apply(
-                                                                                                NotesViewColumnNamesPostProcessor.Apply(
-                                                                                                    NotesDatabaseLifecyclePostProcessor.Apply(
-                                                                                                        NotesDatabaseLotusScriptSurfacePostProcessor.Apply(
-                                                                                                            NotesDocumentCollectionPostProcessor.Apply(
-                                                                                                                NotesRuntimeCoreSource.Code + "\n\n" +
-                                                                                                                NotesRuntimeValueSource.Code + "\n\n" +
-                                                                                                                NotesRuntimeDataSource.Code + "\n\n" +
-                                                                                                                NotesRuntimeItemSource.Code + "\n\n" +
-                                                                                                                NotesRuntimeIndexedValueSource.Code + "\n\n" +
-                                                                                                                NotesNativeApiSource.Code)))))))))))))))))))))))));
+                    NotesRichTextAttachmentInsertPostProcessor.Apply(
+                        NotesEmbeddedObjectPostProcessor.Apply(
+                            NotesRichTextRangePostProcessor.Apply(
+                                NotesRichTextObjectsPostProcessor.Apply(
+                                    NotesRichTextMimePostProcessor.Apply(
+                                        NotesThreadLifecyclePostProcessor.Apply(
+                                            NotesDxlPostProcessor.Apply(
+                                                NotesDocumentComputeWithFormPostProcessor.Apply(
+                                                    NotesDocumentLotusScriptSurfacePostProcessor.Apply(
+                                                        NotesDocumentRemovePostProcessor.Apply(
+                                                            NotesDatabaseCreateCompatibilityPostProcessor.Apply(
+                                                                NotesViewNavigatorHistoryCapPostProcessor.Apply(
+                                                                    NotesViewNavigatorBufferMaxEntriesPostProcessor.Apply(
+                                                                        NotesViewNavigatorCachePostProcessor.Apply(
+                                                                            NotesViewNavigatorCachePolicyPostProcessor.Apply(
+                                                                                NotesViewNavigationV3FixPostProcessor.Apply(
+                                                                                    NotesViewNavigationV3PostProcessor.Apply(
+                                                                                        NotesViewNavigationV2FixPostProcessor.Apply(
+                                                                                            NotesViewNavigationV2PostProcessor.Apply(
+                                                                                                NotesViewNavigationPostProcessor.Apply(
+                                                                                                    NotesViewColumnNamesPostProcessor.Apply(
+                                                                                                        NotesDatabaseLifecyclePostProcessor.Apply(
+                                                                                                            NotesDatabaseLotusScriptSurfacePostProcessor.Apply(
+                                                                                                                NotesDocumentCollectionPostProcessor.Apply(
+                                                                                                                    NotesRuntimeCoreSource.Code + "\n\n" +
+                                                                                                                    NotesRuntimeValueSource.Code + "\n\n" +
+                                                                                                                    NotesRuntimeDataSource.Code + "\n\n" +
+                                                                                                                    NotesRuntimeItemSource.Code + "\n\n" +
+                                                                                                                    NotesRuntimeIndexedValueSource.Code + "\n\n" +
+                                                                                                                    NotesNativeApiSource.Code))))))))))))))))))))))))));
             NotesViewNavigatorCachePolicyRegression.Validate(source);
             return source;
         }
