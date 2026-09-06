3cb9ec9708a8aea8fc1120616970bd0ac7f5c78a
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-06 03:18:00 +0200

    Refresh document collection normalization anchor

--- src/XPScript.Compiler/NotesDocumentCollectionNormalizationPostProcessor.cs
+++ src/XPScript.Compiler/NotesDocumentCollectionNormalizationPostProcessor.cs
@@ -9,7 +9,7 @@ internal static class NotesDocumentCollectionNormalizationPostProcessor
         const string oldRemoveIds = """
     private void RemoveIds(IEnumerable<uint> noteIds)
     {
-        var remove = noteIds as HashSet<uint> ?? new HashSet<uint>(noteIds.Select(id => id & 0x7fffffffu));
+        var remove = new HashSet<uint>(noteIds.Select(id => id & 0x7fffffffu));
         if (remove.Count == 0) return;
         _noteIds = _noteIds.Where(id => !remove.Contains(id)).ToArray();
         ResetLastFetched();
