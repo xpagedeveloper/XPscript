9565cd62f09df26a328b7870eccb084fbf34f5fb
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-07 20:57:23 +0200

    Apply Notes MIME ABI validation

--- src/XPScript.Compiler/NotesExtendedRuntimePostProcessor.cs
+++ src/XPScript.Compiler/NotesExtendedRuntimePostProcessor.cs
@@ -7,6 +7,7 @@ internal static class NotesExtendedRuntimePostProcessor
         ArgumentNullException.ThrowIfNull(source);
         source = NotesStreamPostProcessor.ApplyBuiltSurface(source);
         source = NotesMimeEntityPostProcessor.ApplyBuiltSurface(source);
+        source = NotesMimeEntityAbiPostProcessor.ApplyBuiltSurface(source);
         source = NotesDocumentMetadataPostProcessor.ApplyBuiltSurface(source);
         source = NotesDocumentAuthorsPostProcessor.ApplyBuiltSurface(source);
         source = NotesSigningPostProcessor.ApplyBuiltSurface(source);
