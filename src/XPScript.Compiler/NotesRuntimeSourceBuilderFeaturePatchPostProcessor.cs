2e8b79f2f55f3ea7cd02c060eac4581567a5eb34
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-07 21:46:56 +0200

    Remove temporary runtime feature marker

--- src/XPScript.Compiler/NotesRuntimeSourceBuilderFeaturePatchPostProcessor.cs
+++
@@ -1,8 +0,0 @@
-namespace XPScript.Compiler;
-
-internal static class NotesRuntimeSourceBuilderFeaturePatchPostProcessor
-{
-    // Marker type for the MIME/RichText runtime feature split.
-    // RuntimeSourceBuilder itself must pass NotesRuntimeFeatures directly to
-    // NotesExtendedRuntimePostProcessor before this helper can be removed.
-}
