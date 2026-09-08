9e7c3218337f1e3846f626d2b4453075006472c6
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-08 08:40:11 +0200

    Invalidate MIME directory before MIME stream writes

--- src/XPScript.Compiler/NotesMimeDocumentLifecyclePostProcessor.cs
+++ src/XPScript.Compiler/NotesMimeDocumentLifecyclePostProcessor.cs
@@ -8,7 +8,7 @@ internal static class NotesMimeDocumentLifecyclePostProcessor
 
         source = ReplaceRequired(source,
             "    public void CloseMIMEEntities() { EnsureAlive(); }",
-            "    private XPScriptNotesMimeDirectoryOwner? _mimeDirectoryOwner;\n\n    internal XPScriptNotesMimeDirectoryOwner GetMimeDirectoryOwner()\n    {\n        EnsureAlive();\n        return _mimeDirectoryOwner ??= new XPScriptNotesMimeDirectoryOwner(Session.Api, checked((uint)_handle));\n    }\n\n    private void ReleaseMimeDirectory()\n    {\n        var owner = _mimeDirectoryOwner;\n        _mimeDirectoryOwner = null;\n        owner?.Dispose();\n    }\n\n    public void CloseMIMEEntities()\n    {\n        EnsureAlive();\n        ReleaseMimeDirectory();\n    }",
+            "    private XPScriptNotesMimeDirectoryOwner? _mimeDirectoryOwner;\n\n    internal XPScriptNotesMimeDirectoryOwner GetMimeDirectoryOwner()\n    {\n        EnsureAlive();\n        return _mimeDirectoryOwner ??= new XPScriptNotesMimeDirectoryOwner(Session.Api, checked((uint)_handle));\n    }\n\n    internal void InvalidateMimeDirectory()\n    {\n        ReleaseMimeDirectory();\n    }\n\n    private void ReleaseMimeDirectory()\n    {\n        var owner = _mimeDirectoryOwner;\n        _mimeDirectoryOwner = null;\n        owner?.Dispose();\n    }\n\n    public void CloseMIMEEntities()\n    {\n        EnsureAlive();\n        ReleaseMimeDirectory();\n    }",
             "document-close-mime-entities");
 
         source = ReplaceRequired(source,
@@ -20,6 +20,11 @@ internal static class NotesMimeDocumentLifecyclePostProcessor
             "    protected override void ReleaseOwnedNative()\n    {\n        var handle = Interlocked.Exchange(ref _handle, 0);\n        if (handle != 0) Session.Api.CloseNote(handle);\n    }",
             "    protected override void ReleaseOwnedNative()\n    {\n        ReleaseMimeDirectory();\n        var handle = Interlocked.Exchange(ref _handle, 0);\n        if (handle != 0) Session.Api.CloseNote(handle);\n    }",
             "document-recycle-closes-mime-entities");
+
+        source = ReplaceRequired(source,
+            "    private void Commit()\n    {\n        _raw = _message.Serialize();\n        Session.Api.WriteMimeStream(_document.NativeHandle, _itemName, _raw);\n        _message = XPScriptMimeMessage.Parse(_raw);\n    }",
+            "    private void Commit()\n    {\n        _raw = _message.Serialize();\n        _document.InvalidateMimeDirectory();\n        Session.Api.WriteMimeStream(_document.NativeHandle, _itemName, _raw);\n        _message = XPScriptMimeMessage.Parse(_raw);\n    }",
+            "mime-write-invalidates-directory");
 
         return source;
     }
