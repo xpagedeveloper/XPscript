4aaba7f936235051283a50649aafa4d9e0e8e0f3
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-09 16:52:17 +0200

    Wire direct child MIME header lookup

--- src/XPScript.Compiler/NotesExtendedRuntimePostProcessor.cs
+++ src/XPScript.Compiler/NotesExtendedRuntimePostProcessor.cs
@@ -31,6 +31,7 @@ internal static class NotesExtendedRuntimePostProcessor
             source = NotesMimeRootMutationPostProcessor.ApplyBuiltSurface(source);
             source = NotesMimeChildMutationPostProcessor.ApplyBuiltSurface(source);
             source = NotesMimeEmptyChildBootstrapPostProcessor.ApplyBuiltSurface(source);
+            source = NotesMimeChildHeaderReadPostProcessor.ApplyBuiltSurface(source);
         }
 
         source = NotesDocumentMetadataPostProcessor.ApplyBuiltSurface(source);
