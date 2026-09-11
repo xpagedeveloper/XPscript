0800ee22c44546889a2d73bc7166f56f75c67764
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-11 23:32:46 +0200

    Normalize extended Archive runtime errors

--- src/XPScript.Compiler/ArchiveExtendedWriterFactoryRuntimeSource.cs
+++ src/XPScript.Compiler/ArchiveExtendedWriterFactoryRuntimeSource.cs
@@ -8,9 +8,9 @@ internal static class XPScriptExtendedArchiveWriterFactory
     public static object Create(object? value)
     {
         if (value is byte[] || value is LSArray)
-            return new XPScriptExtendedMemoryArchiveV3(value);
-        return new XPScriptExtendedArchiveV4(value);
+            return new XPScriptExtendedMemoryArchiveV4(value);
+        return new XPScriptExtendedArchiveV5(value);
     }
 }
-""" + "\n" + ArchiveCompressedTarRuntimeSource.Code + "\n" + ArchiveDetectedFormatRuntimeSource.Code + "\n" + ArchiveExtendedMemoryRuntimeSource.Code + "\n" + ArchiveExtendedMemoryWriterRuntimeSource.Code + "\n" + ArchiveExtendedMemoryRebuildRuntimeSource.Code;
+""" + "\n" + ArchiveCompressedTarRuntimeSource.Code + "\n" + ArchiveDetectedFormatRuntimeSource.Code + "\n" + ArchiveExtendedMemoryRuntimeSource.Code + "\n" + ArchiveExtendedMemoryWriterRuntimeSource.Code + "\n" + ArchiveExtendedMemoryRebuildRuntimeSource.Code + "\n" + ArchiveErrorNormalizationRuntimeSource.Code;
 }
