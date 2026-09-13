cbe326205e506eacebcc52a7a8923e00ee919bec
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-13 09:56:27 +0200

    Apply NotesDocument.Send runtime surface

--- src/XPScript.Compiler/NotesRuntimeSource.cs
+++ src/XPScript.Compiler/NotesRuntimeSource.cs
@@ -39,6 +39,7 @@ internal static class NotesRuntimeSource
         source = NotesSessionEnvironmentPostProcessor.Apply(source);
         source = NotesSessionPasswordPostProcessor.Apply(source);
         source = NotesSessionAddressBooksPostProcessor.Apply(source);
+        source = NotesDocumentSendPostProcessor.Apply(source);
         source = NotesDocumentRemovePostProcessor.Apply(source);
         source = NotesDocumentLotusScriptSurfacePostProcessor.Apply(source);
         source = NotesDocumentComputeWithFormPostProcessor.Apply(source);
