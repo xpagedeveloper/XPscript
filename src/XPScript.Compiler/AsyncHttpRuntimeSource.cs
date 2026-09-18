4ad7b457f2ddfffb4bc50e7c525764880e49cbee
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-18 06:32:54 +0200

    Inject HTTP core runtime only once

--- src/XPScript.Compiler/AsyncHttpRuntimeSource.cs
+++ src/XPScript.Compiler/AsyncHttpRuntimeSource.cs
@@ -164,5 +164,5 @@ internal sealed class XPScriptHttpAsyncRequest
         return true;
     }
 }
-""" + "\n" + AiToolRuntimeSource.Code + "\n" + HttpCoreRuntimeSource.Code;
+""" + "\n" + AiToolRuntimeSource.Code;
 }
