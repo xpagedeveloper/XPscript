85a5bb87ca821025242290b0bc7dc46b81e4d376
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-18 21:57:23 +0200

    compiler: migrate source diagnostic code field

--- src/XPScript.Compiler/CompilerDiagnosticParser.cs
+++ src/XPScript.Compiler/CompilerDiagnosticParser.cs
@@ -28,7 +28,7 @@ internal static class CompilerDiagnosticParser
                 Line = line,
                 Position = pos,
                 Description = Humanize(match.Groups["desc"].Value.Trim()),
-                Code = code,
+                SourceCode = code,
                 MarkedCode = Mark(code, pos)
             });
         }
