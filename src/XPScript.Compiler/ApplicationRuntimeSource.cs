23ec81d39da26d1c7a8302fb3bf444edbb4d5c89
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-20 18:33:02 +0200

    Include application logging runtime

--- src/XPScript.Compiler/ApplicationRuntimeSource.cs
+++ src/XPScript.Compiler/ApplicationRuntimeSource.cs
@@ -297,5 +297,5 @@ internal static class XPScriptApplicationRuntime
     public static string Path => ExecutablePath;
     public static string FileName => ExecutableFileName;
 }
-""" + ApplicationPersistenceRuntimeSource.Code + ApplicationSystemLogRuntimeSource.Code;
+""" + ApplicationPersistenceRuntimeSource.Code + ApplicationSystemLogRuntimeSource.Code + ApplicationLogRuntimeSource.Code;
 }
