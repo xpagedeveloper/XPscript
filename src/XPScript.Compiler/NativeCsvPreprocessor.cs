502627e80d61afe6d570f8777e0b083409cf78bc
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-21 23:51:56 +0200

    Use shared scoped rewriter for CSV runtime calls

--- src/XPScript.Compiler/NativeCsvPreprocessor.cs
+++ src/XPScript.Compiler/NativeCsvPreprocessor.cs
@@ -65,10 +65,10 @@ internal sealed class NativeCsvPreprocessor
             rewritten = Regex.Replace(rewritten, @"\bXPCsvDocument\.Load\s*\(([^)]*)\)", m => RewriteLoad(m.Groups[1].Value), RegexOptions.IgnoreCase);
             rewritten = Regex.Replace(rewritten, @"\bXPCsvDocument\.ParseBytes\s*\(", "XPScriptNativeCsv.ParseBytes(", RegexOptions.IgnoreCase);
             rewritten = Regex.Replace(rewritten, @"\bXPCsvDocument\.Parse\s*\(", "XPScriptNativeCsv.Parse(", RegexOptions.IgnoreCase);
-            rewritten = Regex.Replace(rewritten, @"(?<![\w.])CsvParseBytes\s*\(", "XPScriptNativeCsv.ParseBytes(", RegexOptions.IgnoreCase);
-            rewritten = Regex.Replace(rewritten, @"(?<![\w.])CsvParse\s*\(", "XPScriptNativeCsv.Parse(", RegexOptions.IgnoreCase);
-            rewritten = Regex.Replace(rewritten, @"(?<![\w.])CsvStringify\s*\(", "XPScriptNativeCsv.Stringify(", RegexOptions.IgnoreCase);
-            rewritten = Regex.Replace(rewritten, @"(?<![\w.])CsvEscape\s*\(", "XPScriptNativeCsv.Escape(", RegexOptions.IgnoreCase);
+            rewritten = PreprocessorFeatureGate.ReplaceUnqualifiedCalls(rewritten, "CsvParseBytes", "XPScriptNativeCsv.ParseBytes");
+            rewritten = PreprocessorFeatureGate.ReplaceUnqualifiedCalls(rewritten, "CsvParse", "XPScriptNativeCsv.Parse");
+            rewritten = PreprocessorFeatureGate.ReplaceUnqualifiedCalls(rewritten, "CsvStringify", "XPScriptNativeCsv.Stringify");
+            rewritten = PreprocessorFeatureGate.ReplaceUnqualifiedCalls(rewritten, "CsvEscape", "XPScriptNativeCsv.Escape");
             rewritten = Regex.Replace(rewritten, @"\bNew\s+XPCsvDocument\s*(?:\(\s*\))?", "XPScriptNativeCsv.CreateDocument()", RegexOptions.IgnoreCase);
 
             foreach (var documentVariable in documentVariables)
