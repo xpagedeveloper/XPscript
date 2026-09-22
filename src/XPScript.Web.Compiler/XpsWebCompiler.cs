37547896e6592fb343d1c2836a04414bf0895506
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-23 00:10:09 +0200

    merge: reconcile main web compiler changes

--- src/XPScript.Web.Compiler/XpsWebCompiler.cs
+++ src/XPScript.Web.Compiler/XpsWebCompiler.cs
@@ -103,7 +103,7 @@ public sealed class XpsWebCompiler
 
         if (parsed.Routes.Count == 0) throw new XpsWebCompilationException("Web source must export at least one route using web route attributes.");
 
-        var compilerSource = EnsureCompilerEntryPoint(NormalizeVariantSetAssignments(parsed.Source));
+        var compilerSource = EnsureCompilerEntryPoint(NormalizeVariantSetAssignments(parsed.Source));\n        if (parsed.Routes.Values.Any(route => route.JsonSchema is not null))\n            compilerSource += "\\nDim __xps_rest_json_schema_runtime As XPJsonSchema\\n";
         string generated;
         try
         {
