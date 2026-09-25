7422ade4081871e19666dd3382842b29604b8642
Fredrik Norling <fredrik@xpagedeveloper.com>
2026-09-26 00:43:14 +0200

    Expose XPImage metadata members

--- src/XPScript.Compiler/ImageObjectPreprocessor.cs
+++ src/XPScript.Compiler/ImageObjectPreprocessor.cs
@@ -8,7 +8,7 @@ internal sealed class ImageObjectPreprocessor
     [
         "Width", "Height", "Format", "DpiX", "DpiY", "Clone", "Resize", "Crop", "Rotate",
         "FlipHorizontal", "FlipVertical", "Brightness", "Contrast", "Saturation", "Grayscale", "Invert", "Blur", "Sharpen",
-        "AutoOrient", "StripMetadata", "ToBytes", "Save", "Dispose", "Pad", "Composite", "Opacity", "Flatten"
+        "AutoOrient", "GetExif", "SetExif", "GetMetadata", "SetMetadata", "StripMetadata", "ToBytes", "Save", "Dispose", "Pad", "Composite", "Opacity", "Flatten"
     ];
 
     public string Transform(string source)
